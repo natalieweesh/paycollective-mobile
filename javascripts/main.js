68bae09750b56108b6848dd7233d06a873e8b9fa
natalieweesh <natalieweesh@gmail.com>
2014-04-24 00:39:27 -0400

    Add messaging-page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+$('.accordion').on('click', function(){
+    $(this).toggleClass('expanded');
+});
+ 
+$('.toggle-pop-up').on('click', function(){
+    $('body').toggleClass('show-pop-up');
+});
+
+$('.swipebox').swipebox({
+    beforeOpen: function() {$('.content').css('position', 'fixed');}, // called before opening
+    afterClose: function() {$('.content').css('position', 'static');} // called after closing
+});
+
+$('.hamburger').on('click', function(){
+    $('.header-menu').toggleClass('expanded');
+});
