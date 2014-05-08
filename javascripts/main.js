59cfa8c1d6dc3c385101e8593e0964509512983a
natalieweesh <natalieweesh@gmail.com>
2014-05-08 10:55:34 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,10 +8,12 @@ $('.toggle-pop-up').on('click', function(){
     $('body').toggleClass('show-pop-up');
 });
 
-$('.swipebox').swipebox({
-    beforeOpen: function() {$('.content').css('position', 'fixed');}, // called before opening
-    afterClose: function() {$('.content').css('position', 'static');} // called after closing
-});
+if ($('.swipebox').length != 0) {
+    $('.swipebox').swipebox({
+        beforeOpen: function() {$('.content').css('position', 'fixed');}, // called before opening
+        afterClose: function() {$('.content').css('position', 'static');} // called after closing
+    });
+}
 
 $('.hamburger').on('click', function(){
     alert('hamburger clicked!');
